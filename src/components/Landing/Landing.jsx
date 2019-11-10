1edf68c866753bf1b0bbb8d0dacf657a81087f79
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2019-11-10 16:22:20 -0600

    Update

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import './Landing.css';
 
 export class Landing extends Component {
+  //api calls here
   render() {
-    console.log(this.props.photos[0].color);
-    return <div className='landing-component'></div>;
+    if (!this.props.photos.length) return null;
+    return (
+      <div>
+        {this.props.photos.map(photo => {
+          console.log(photo);
+          return (
+            <div className='landing-component-image'>
+              <img src={photo.urls.small} />
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
-
 export default Landing;
+
+/*  <div>
+ <img src={photo.user.profile_image.small} />
+ <div>{photo.user.username}</div> */
