342d8936ddb8165c76a62d369e0c84fad6fb8394
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2019-11-17 06:34:15 -0600

    Create route functionality for search bar to user, component, and photo search pages using withRouter

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,43 +1,68 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import MagnifyingGlassIcon from '../../assets/magnifying-glass.png';
 import './SearchBarStyles.css';
 
 export class SearchBar extends Component {
   state = {
-    searchTerm: ''
+    searchTerm: '',
+    category: ''
   };
-  handleChange = e => this.setState({ searchTerm: e.target.value });
+
+  handleSearchChange = e =>
+    this.setState({ searchTerm: e.target.value.toLowerCase() });
+  handleCategoryChange = e => {
+    console.log(e.target.value);
+    this.setState({ category: e.target.value.toLowerCase() });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.searchTerm) {
+      this.props.history.push(
+        `/${this.state.category}/${this.state.searchTerm}`
+      );
+    }
+  };
 
   render() {
     return (
-      <div className='search-bar-component'>
-        <form>
-          <div className='dropdown'>
-            <button className='dropbtn'>Search Category</button>
-            <div className='dropdown-content'>
-              <a href='#'>Users</a>
-              <a href='#'>Collections</a>
-              <a href='#'>Photos</a>
-            </div>
-          </div>
-
-          <input
-            onChange={this.handleChange}
-            type='text'
-            className='search-bar-input'
-            placeholder='Search'
+      <form onSubmit={this.handleSubmit} className='search-bar-component'>
+        <select
+          value=''
+          onChange={this.handleCategoryChange}
+          id='search-category-select'>
+          <option value='' disabled hidden>
+            Category
+          </option>
+          <option value='Users'>Users</option>
+          <option value='Collections'>Collections</option>
+          <option value='Photos'>Photos</option>
+        </select>
+        <input
+          onChange={this.handleSearchChange}
+          type='text'
+          className='search-bar-input'
+          placeholder='Search'
+        />
+        <button className='magnifying-glass-div'>
+          <img
+            src={MagnifyingGlassIcon}
+            className='magnifying-glass-icon'
+            alt='magnifying-glass'
           />
-          <div className='magnifying-glass-div'>
-            <img
-              src={MagnifyingGlassIcon}
-              className='magnifying-glass-icon'
-              alt='magnifying-glass'
-            />
-          </div>
-        </form>
-      </div>
+        </button>
+      </form>
     );
   }
 }
 
-export default SearchBar;
+export default withRouter(SearchBar);
+
+/* <div className='dropdown'>
+<div className='dropbtn'>Search Category</div>
+<div className='dropdown-content'>
+  <a onClick={this.handleCategoryChange}>Users</a>
+  <a onClick={this.handleCategoryChange}>Collections</a>
+  <a onClick={this.handleCategoryChange}>Photos</a>
+</div>
+</div> */
