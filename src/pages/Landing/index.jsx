63cb82e41fb7de49e8e426dfd8084906e06256a5
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2019-11-24 10:12:22 -0600

    Initial commit

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -12,7 +12,6 @@ export class Landing extends Component {
   };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.onScroll);
     this.getData();
   }
 
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
 import UserInfoComponent from '../../components/UserInfo';
-import Photo from '../../components/Photo';
-
 import { toJson } from 'unsplash-js';
-
+import InfiniteScroll from 'react-infinite-scroller';
 import './UserStyles.css';
-
+import Photo from '../../components/Photo';
 import { unsplash } from '../../unsplash';
 
 class UserPage extends Component {
   state = {
-    photos: []
+    photos: [],
+    page: 1
   };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.onScroll);
     this.getData();
   }
 
   getData() {
+    console.log(this.props.match.userName);
     unsplash.users
-      .photos(this.props.match.params.userName, 1, 10, 'popular', false)
+      .photos(
+        this.props.match.params.userName,
+        this.state.page,
+        10,
+        'popular',
+        false
+      )
       .then(toJson)
       .then(json => {
         this.setState({
-          photos: json
+          photos: [...this.state.photos, ...json],
+          page: this.state.page + 1,
+          hasMore: !!json.length
         });
       });
   }
@@ -37,9 +44,19 @@ class UserPage extends Component {
     return (
       <div className='user-page-component'>
         <UserInfoComponent user={user} />
-        {photos.map(photo => {
-          return <Photo key={photo.id} photo={photo} />;
-        })}
+        <InfiniteScroll
+          pageStart={1}
+          loadMore={this.getData}
+          hasMore={true || false}
+          loader={
+            <div className='loader' key={0}>
+              Loading ...
+            </div>
+          }>
+          {photos.map(photo => {
+            return <Photo key={photo.id} photo={photo} />;
+          })}
+        </InfiniteScroll>
       </div>
     );
   }
