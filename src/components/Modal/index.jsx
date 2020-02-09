3030cc061e12b3f820799774fb269c9131cacbfe
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-02-08 22:21:52 -0600

    Fix modal to be full size and have the correct aspect ratio

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -3,42 +3,47 @@ import styled, { css } from 'styled-components';
 
 const Container = styled.div`
   position: fixed;
-  border: solid black 1px;
   display: flex;
   justify-content: center;
-  align-items: center;
-  left: 35%;
+  border: solid black 1px;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   z-index: 11;
-  background-color: rgba(0, 0, 0, 0.45);
+  background-color: rgba(0, 0, 0, 0.8);
   transition: 0.3s;
+  text-align: center;
 `;
 
-const Container2 = styled.div`
+const ModalImage = styled.div`
   position: relative;
+  margin: 0 auto;
+  width: auto;
+  height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
 `;
 
 const Img = styled.img`
-  display: block;
   ${props =>
     props.landscape
       ? css`
-          width: 80vw;
+          width: 100%;
         `
       : css`
-          height: 80vh;
-        `}
+          height: 100%;
+        `} /* Set rules to fill background */
+
+
+  /* Set up proportionate scaling */
+
+  /* Set up positioning */
 `;
 
 const Close = styled.span`
   position: absolute;
-  right: 0;
+  left: 0;
   top: 0;
   padding: 20px;
   -webkit-text-fill-color: white;
@@ -57,21 +62,25 @@ const Modal = props => {
   if (!props.show) {
     return null;
   }
-  console.log(props);
   return (
-    <Container id='container1'>
-      <Container2 id='container2'>
+    <Container id='container'>
+      <ModalImage id='modal-image'>
         <Img
-          landscape={props.landscape}
           src={props.photo}
           alt='nada'
           onClick={props.onClose}
-        />
-
+          landscape={props.landscape}></Img>
         <Close onClick={props.onClose}>&times;</Close>
-      </Container2>
+      </ModalImage>
     </Container>
   );
 };
+
+/*<Img
+landscape={props.landscape}
+src={props.photo}
+alt='nada'
+onClick={props.onClose}
+/>*/
 
 export default Modal;
