8c8b18eb05d0c34d7b67534773c6ff4d008645c8
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-18 00:41:07 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -17,77 +17,65 @@ const FourOhFour = () => <div>Not Found</div>;
 export const LayoutContext = React.createContext();
 
 class App extends React.Component {
-  //state for grid/list/column view
-  state = {
-    isGrid: true,
-    isList: false,
-    isColumn: false,
-  };
+	//state for grid/list/column view
+	state = {
+		isGrid: true,
+		isList: false,
+		isColumn: false
+	};
 
-  toggleView = (e) => {
-    //get current values from state and copy it
-    const currentValues = { ...this.state }; //spread the state object
-    //set all copied state values to false
-    Object.keys(currentValues).forEach((prop) => {
-      currentValues[prop] = false;
-    });
-    const gridSettings = {
-      //paste copy back into state & update the current target.value
-      ...currentValues, //spread the object
-      [e.target.name]: true,
-    };
-    this.setState(gridSettings);
+	toggleView = (e) => {
+		//get current values from state and copy it
+		const currentValues = { ...this.state }; //spread the state object
+		//set all copied state values to false
+		Object.keys(currentValues).forEach((prop) => {
+			currentValues[prop] = false;
+		});
+		const gridSettings = {
+			//paste copy back into state & update the current target.value
+			...currentValues, //spread the object
+			[e.target.name]: true
+		};
+		this.setState(gridSettings);
 
-    localStorage.setItem('gridSettings', JSON.stringify(gridSettings));
-  };
+		localStorage.setItem('gridSettings', JSON.stringify(gridSettings));
+	};
 
-  componentDidMount() {
-    const gridSettings = localStorage.getItem('gridSettings');
-    if (gridSettings) this.setState(JSON.parse(gridSettings));
-  }
+	componentDidMount() {
+		const gridSettings = localStorage.getItem('gridSettings');
+		if (gridSettings) this.setState(JSON.parse(gridSettings));
+	}
 
-  render() {
-    return (
-      <LayoutContext.Provider
-        value={{
-          isGrid: this.state.isGrid,
-          isList: this.state.isList,
-          isColumn: this.state.isColumn,
-          toggleView: this.toggleView,
-        }}>
-        <Router>
-          <div className='app-container'>
-            <GoToTop />
-            <HeaderComponent />
-            <div className='routes-container'>
-              <Switch>
-                <Route path='/' exact component={LandingPage} />
-                <Route path='/user/:userName' exact component={UserPage} />
-                <Route
-                  path='/search/users/:user'
-                  exact
-                  component={UserSearch}
-                />
-                <Route
-                  path='/search/collections/:collection'
-                  exact
-                  component={CollectionsSearch}
-                />
-                <Route path='/collection/:id' exact component={Collection} />
-                <Route
-                  path='/search/photos/:searchTerm'
-                  exact
-                  component={PhotosSearch}
-                />
-                <Route path='/favorites' exact component={Favorites} />
-                <Route path='*' component={FourOhFour} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      </LayoutContext.Provider>
-    );
-  }
+	render() {
+		return (
+			<LayoutContext.Provider
+				value={{
+					isGrid: this.state.isGrid,
+					isList: this.state.isList,
+					isColumn: this.state.isColumn,
+					toggleView: this.toggleView
+				}}>
+				<Router>
+					<div className='app-container'>
+						<GoToTop />
+						<HeaderComponent />
+						<div className='routes-container'>
+							<Switch>
+								<Route path='/' exact component={LandingPage} />
+								<Route path='/user/:userName' exact component={UserPage} />
+								<Route path='/search/users/:user' exact component={UserSearch} />
+								<Route path='/search/collections/:collection' exact component={CollectionsSearch} />
+								<Route path='/collection/:id' exact component={Collection} />
+								<Route path='/search/photos/:searchTerm' exact component={PhotosSearch} />
+								<Route path='/favorites' exact component={Favorites} />
+								<Route path='*' component={FourOhFour} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</LayoutContext.Provider>
+		);
+	}
 }
 
 export default App;
--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -8,88 +8,88 @@ import styled, { css } from 'styled-components';
 export const CategoryContext = React.createContext();
 
 const Wrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  min-width: 75px;
-  outline: none;
-  border-radius: 0 0 3px 3px;
-  &:hover div {
-    display: block;
-  }
+	position: relative;
+	display: inline-block;
+	min-width: 75px;
+	outline: none;
+	border-radius: 0 0 3px 3px;
+	&:hover div {
+		display: block;
+	}
 `;
 
 const DropButton = styled.button`
-  background-color: rgb(250, 250, 250);
-  color: rgb(0, 0, 0);
-  padding: 7px 12px 7px 12px;
-  font-size: 16px;
-  width: 100px;
-  border: solid lightgrey 1px;
-  outline: none;
-  border-top: red;
-  border-bottom: red;
-  border-left: red;
-  border-radius: 3px 0 0 3px;
-  &:hover {
-    border-radius: 3px 0 0 0;
-  }
+	background-color: rgb(250, 250, 250);
+	color: rgb(0, 0, 0);
+	padding: 7px 12px 7px 12px;
+	font-size: 16px;
+	width: 100px;
+	border: solid lightgrey 1px;
+	outline: none;
+	border-top: red;
+	border-bottom: red;
+	border-left: red;
+	border-radius: 3px 0 0 3px;
+	&:hover {
+		border-radius: 3px 0 0 0;
+	}
 `;
 const DropdownContent = styled.div`
-  display: none;
-  position: absolute;
-  background-color: rgb(250, 250, 250);
-  min-width: 75px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  border-radius: 0 0 3px 3px;
-  z-index: 1;
+	display: none;
+	position: absolute;
+	background-color: rgb(250, 250, 250);
+	min-width: 75px;
+	box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+	border-radius: 0 0 3px 3px;
+	z-index: 1;
 `;
 
 const Button = styled.button`
-  color: black;
-  padding: 10px;
-  font-size: 1rem;
-  text-decoration: none;
-  display: block;
-  border: none;
-  outline: none;
-  width: 100%;
-  &:hover {
-    background-color: rgb(255, 0, 0, 0.8);
-  }
-  &:active {
-    background-color: rgb(255, 0, 0, 0.3);
-  }
-  &:last-child {
-    border-radius: 0 0 3px 3px;
-  }
+	color: black;
+	padding: 10px;
+	font-size: 1rem;
+	text-decoration: none;
+	display: block;
+	border: none;
+	outline: none;
+	width: 100%;
+	&:hover {
+		background-color: rgb(255, 0, 0, 0.8);
+	}
+	&:active {
+		background-color: rgb(255, 0, 0, 0.3);
+	}
+	&:last-child {
+		border-radius: 0 0 3px 3px;
+	}
 `;
 
 const Dropdown = (props) => {
-  const [category, setCategory] = useState('Category');
+	const [ category, setCategory ] = useState('Category');
 
-  const handleClick = (e) => {
-    console.log(e.target.value);
-    setCategory(e.target.value);
-    props.handleCategoryChange(e.target.value);
-  };
+	const handleClick = (e) => {
+		console.log(e.target.value);
+		setCategory(e.target.value);
+		props.handleCategoryChange(e.target.value);
+	};
 
-  return (
-    <Wrapper class='dropdown'>
-      <DropButton id='dropbtn' class='dropbtn'>
-        {category}
-      </DropButton>
-      <DropdownContent class='dropdown-content'>
-        <Button id='btn1' onClick={handleClick} value='Users'>
-          Users
-        </Button>
-        <Button id='btn2' onClick={handleClick} value='Collections'>
-          Collections
-        </Button>
-        <Button id='btn3' onClick={handleClick} value='Photos'>
-          Photos
-        </Button>
-      </DropdownContent>
-    </Wrapper>
-  );
+	return (
+		<Wrapper className='dropdown'>
+			<DropButton id='dropbtn' className='dropbtn'>
+				{category}
+			</DropButton>
+			<DropdownContent className='dropdown-content'>
+				<Button id='btn1' onClick={handleClick} value='Users'>
+					Users
+				</Button>
+				<Button id='btn2' onClick={handleClick} value='Collections'>
+					Collections
+				</Button>
+				<Button id='btn3' onClick={handleClick} value='Photos'>
+					Photos
+				</Button>
+			</DropdownContent>
+		</Wrapper>
+	);
 };
 export default Dropdown;
